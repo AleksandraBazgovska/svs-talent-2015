dc21225887d82c903983a489ff8c15c8c8aaceb6
Aleksandra Bazgovska <aleksandrabazgovska@gmail.com>
2015-04-08 13:16:42 +0200

    WCF Service Domashno
    Folder za domashno od WCF

---
+++ WCF Service Domashno/WcfServiceLibrary1/ShowEmployeeSite/ShowEmployee.aspx.cs
@@ -0,0 +1,57 @@
+﻿using ShowEmployeeSite.EmployeeService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShowEmployeeSite
+{
+    public partial class ShowEmployee : System.Web.UI.Page
+    {
+        Service1Client employeeSvc = new Service1Client();// iNstanciranje na  Service1client
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {              
+
+                GridView1.DataSource = employeeSvc.GetEmployeesData();
+                GridView1.DataBind();
+            }
+        }
+
+        public static int i = 3;
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            i += 1;
+            Employee objcust =
+            new Employee()
+            {
+                EmployeeID = i,
+                FirstName = TextBox1.Text,
+                LastName = TextBox2.Text,
+                Age = Convert.ToInt32(TextBox3.Text)
+            };
+
+            employeeSvc.InsertEmployee(objcust);
+
+            GridView1.DataSource = employeeSvc.GetEmployeesData();
+            GridView1.DataBind();
+            Label1.Text = "Record Saved Successfully";
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView1.DataKeys
+            [e.RowIndex].Values["EmployeeID"].ToString());
+            employeeSvc = new Service1Client();
+
+            bool check = employeeSvc.DeleteEmployee(userid);
+            Label1.Text = "Record Deleted Successfully";
+            GridView1.DataSource = employeeSvc.GetEmployeesData();
+            GridView1.DataBind();
+        }
+
+    }
+}
---
+++ WCF Service Domashno/WcfServiceLibrary1/WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+
+        /// <summary>
+        /// Lista od site Employee
+        /// </summary>
+     public static  List<Employee> lstEmployees = new List<Employee>(){
+
+                              new Employee{EmployeeID=1,FirstName="Adam", LastName="Daniel",Age=25,},
+
+                              new Employee{EmployeeID=2,FirstName="Calvin",LastName="Klain",Age=35},
+
+                              new Employee{EmployeeID=3,FirstName="Kurt",LastName="Delgado",Age=28}
+
+      };
+
+
+        /// <summary>
+        /// Brishenje na Employee spored Id 
+        /// </summary>
+        /// <param name="Cid"></param>
+        /// <returns></returns>
+
+        public bool DeleteEmployee(int Cid)
+        {
+            var item = lstEmployees.First(x => x.EmployeeID == Cid);
+
+            lstEmployees.Remove(item);
+            return true;
+        }
+
+       
+        /// <summary>
+        /// Vrakja lista od site employees
+        /// </summary>
+        /// <returns></returns>
+        public List<Employee> GetEmployeesData()
+        {
+                        
+
+            return lstEmployees;
+
+        }
+        /// <summary>
+        /// Vnesuvanje na employee vo lista
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool InsertEmployee(Employee obj)
+        {
+            lstEmployees.Add(obj);
+            return true;
+        }
+
+       
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+    }
+}
