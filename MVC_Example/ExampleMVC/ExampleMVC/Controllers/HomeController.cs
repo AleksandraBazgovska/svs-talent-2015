610f53ff5d1bcf0b194958deef6eb6df2e861ed4
AleksandraBazgovska <aleksandrabazgovska@gmail.com>
2015-04-22 23:26:01 +0200

    MVC example
    MVC example finished

---
+++ MVC_Example/ExampleMVC/ExampleMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExampleMVC.Models;
+
+namespace ExampleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<BikeModel> bikes = new List<BikeModel>();
+            bikes.Add(new BikeModel("001", "Producer1", "BMX", "blue"));
+            bikes.Add(new BikeModel("002", "Producer2", "BMX", "blue"));
+
+            bikes.Add(new BikeModel("003", "Producer3", "BMX", "blue"));
+
+            bikes.Add(new BikeModel("004", "Producer4", "BMX", "blue"));
+
+            return View(bikes);
+        }
+    }
+}
---
+++ MVC_Example/ExampleMVC/ExampleMVC/Models/BikeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExampleMVC.Models
+{
+    public class BikeModel
+    {
+        public string RegNumber { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+
+
+        public BikeModel(string regnumber, string producer, string model, string colour)
+        {
+            this.RegNumber = regnumber;
+            this.Producer = producer;
+            this.Model = model;
+            this.Colour = colour;
+        }
+    }
+}
